1cce2a436b85c0e83828e96ff00d091fbfd29286
Moosemello <neilf65@hotmail.com>
2026-04-20 02:45:50 -0400

    Sorted prefabs and Security Camera refinement
    - Organized the Prefab folder
    - Security Camera now works with proper colliders

--- Assets/Scripts/SecurityCam.cs
+++ Assets/Scripts/SecurityCam.cs
@@ -2,33 +2,55 @@ using UnityEngine;
 
 public class SecurityCam : MonoBehaviour
 {
-    GameObject Camera;
+    [SerializeField] private GameObject Player;
     [SerializeField] private float camRotateY;
     [SerializeField] private float camRotateTime = 5f;
-    bool camRotSwap;
-    Transform camRotation;
+    private float zapTime = 0f;
+    private Vector3 camRotationOrigin;
 
+    void Awake()
+    {
+        camRotationOrigin = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
     void Update()
     {
+        RotateObject();
         camRotateTime -= Time.deltaTime;
         var direction = transform.forward;
         RaycastHit hit;
-        Vector3 RayDirection = new Vector3(0, 0, 0);
 
-        if (Physics.Raycast(transform.position, direction, out hit, 50f))
+        if (Physics.Raycast(transform.position, direction, out hit, 10f))
         {
-            PlayerController player = gameObject.GetComponent<PlayerController>();
-            print("Found an object - distance: " + hit.distance);
+            PlayerController player = Player.GetComponent<PlayerController>();
+            // print("Found an object - distance: " + hit.distance);
             Debug.Log(hit.transform.name);
 
-            if (player != null)
-            {
-                player.ChangeHealth(-10);
-                Debug.Log("currentHealth:" + player.currentHealth);
-            }
+            if (hit.collider.CompareTag("Player"))
+                {
+                    transform.LookAt(Player.transform.position);
+                    zapTime += Time.deltaTime;
+
+                    if (player != null && zapTime >= 5f)
+                    {
+                        player.ChangeHealth(-10);
+                        Debug.Log("currentHealth:" + player.currentHealth);
+                        zapTime = 0f;
+                        transform.LookAt(camRotationOrigin);
+                        camRotateTime = 0f;
+                        camRotateY = -0.1f;
+                    }
+
+                    else if (player == null)
+                    {
+                        RotateObject();
+                        zapTime = 0f;
+                        transform.LookAt(camRotationOrigin);
+                        camRotateTime = 0f;
+                        camRotateY = -0.1f;
+                    }
+                }
         }
 
-        RotateObject();
     }
 
     private void RotateObject()
@@ -46,3 +68,7 @@ public class SecurityCam : MonoBehaviour
         }
     }
 }
+
+
+    
+
